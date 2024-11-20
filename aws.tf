1d3951d418332fe979c291241e2e602422933adb
Eike Thaden <ethaden@confluent.io>
2024-11-20 17:51:22 +0000

    Fixed setup

--- aws.tf
+++ aws.tf
@@ -17,10 +17,37 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_vpc_attach" {
   subnet_ids         = toset(data.aws_subnets.vpc_subnets.ids)
   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
   vpc_id             = var.vpc_id
+
+  depends_on = [
+    aws_ram_resource_association.tgw_share_assoc,
+  ]
 }
 
-resource "aws_ec2_transit_gateway_connect" "tgw_connect" {
-  transport_attachment_id = aws_ec2_transit_gateway_vpc_attachment.tgw_vpc_attach.id
-  transit_gateway_id      = aws_ec2_transit_gateway.tgw.id
+# Do not use
+#resource "aws_ec2_transit_gateway_connect" "tgw_connect" {
+#  transport_attachment_id = aws_ec2_transit_gateway_vpc_attachment.tgw_vpc_attach.id
+#  transit_gateway_id      = aws_ec2_transit_gateway.tgw.id
+#}
+
+resource "aws_ram_resource_share" "tgw_share" {
+
+  name =  "${var.resource_prefix}tgw_ram"
+  allow_external_principals = true
 }
 
+# Share the transit gateway...
+resource "aws_ram_resource_association" "tgw_share_assoc" {
+
+  resource_arn       = aws_ec2_transit_gateway.tgw.arn
+  resource_share_arn = aws_ram_resource_share.tgw_share.id
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "tgw_accepter" {
+
+  transit_gateway_attachment_id = confluent_transit_gateway_attachment.confluent_tgw_attach.aws[0].transit_gateway_attachment_id
+
+  tags = {
+    Name = "${var.resource_prefix}tgw_accepter"
+    Side = "Accepter"
+  }
+}
--- confluent.tf
+++ confluent.tf
@@ -34,14 +34,34 @@ resource "confluent_kafka_cluster" "dedicated" {
   }
 }
 
-# resource "confluent_dns_forwarder" "main" {
+resource "aws_ram_principal_association" "confluent" {
+  principal          = confluent_network.net.aws[0].account
+  resource_share_arn = aws_ram_resource_share.tgw_share.arn
+}
+
+resource "confluent_transit_gateway_attachment" "confluent_tgw_attach" {
+  display_name = "AWS Transit Gateway Attachment"
+  aws {
+    ram_resource_share_arn = aws_ram_resource_share.tgw_share.arn
+    transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+    routes                 = ["172.29.0.0/16"]
+  }
+  environment {
+    id = confluent_environment.myenv.id
+  }
+  network {
+    id = confluent_network.net.id
+  }
+}
+
+# resource "confluent_dns_forwarder" "dns_forwarder" {
 #   display_name = "dns_forwarder"
 #   environment {
-#     id = confluent_environment.development.id
+#     id = confluent_environment.myenv.id
 #   }
 #   domains = ["example.com", "domainname.com"]
 #   gateway {
-#     id = confluent_network.main.gateway[0].id
+#     id = confluent_network.net.gateway[0].id
 #   }
 #   forward_via_ip {
 #     dns_server_ips = ["10.200.0.0", "10.200.0.1"]
